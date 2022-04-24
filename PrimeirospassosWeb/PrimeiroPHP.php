500ef9979782470cd37275329a80685d0b530714
Paulosraiva41 <paulosraiva@hotmail.com>
2022-04-24 01:47:20 -0300

    tentativa de atualização

---
+++ PrimeirospassosWeb/PrimeiroPHP.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>PrimeiroPHP</title>
+</head>
+
+    <body>
+    
+    <?php
+        for ($i = 0; $i < 10; $i++) {
+            print ("Numero" . $i . "<br/>");
+        }
+	?>
+    </body>
+   
+</html>
